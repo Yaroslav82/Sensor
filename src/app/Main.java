f8bd2df0f9f6df3db90146094e4aaf4fc1c146c2
Yaroslav82 <yargay230607@gmail.com>
2023-08-30 13:36:35 +0300

    Add sensors for substance

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         Substance substance = new Substance();
+        new WeightSensor(substance);
+        new ColorSensor(substance);
 
         System.out.println("Substance temperature +25 ");
         substance.setState(25);
